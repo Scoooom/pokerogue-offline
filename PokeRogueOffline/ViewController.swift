efa1ffb27ef0f25540e1e8ccdc97d3ded52b0246
Scooom <me@scooom.xyz>
2026-04-02 13:17:30 -0500

    Clean File

--- PokeRogueOffline/ViewController.swift
+++ PokeRogueOffline/ViewController.swift
@@ -2,7 +2,7 @@ import UIKit
 import WebKit
 import Swifter
 
-class ViewController: UIViewController,	WKNavigationDelegate {
+class ViewController: UIViewController, WKNavigationDelegate {
     var webView: WKWebView!
     var server: HttpServer!
 
@@ -17,20 +17,6 @@ class ViewController: UIViewController,	WKNavigationDelegate {
         config.mediaTypesRequiringUserActionForPlayback = []
 
         webView = WKWebView(frame: view.bounds, configuration: config)
-        webView.configuration.userContentController.add(self, name: "logger")
-        let logScript = WKUserScript(
-            source: """
-            window.onerror = function(msg, url, line) {
-                window.webkit.messageHandlers.logger.postMessage('ERROR: ' + msg + ' at ' + url + ':' + line);
-            };
-            console.log = function(msg) {
-                window.webkit.messageHandlers.logger.postMessage('LOG: ' + msg);
-            };
-            """,
-            injectionTime: .atDocumentStart,
-                 forMainFrameOnly: false
-        )
-        webView.configuration.userContentController.addUserScript(logScript)
         webView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         webView.navigationDelegate = self
         webView.scrollView.isScrollEnabled = false
@@ -42,63 +28,58 @@ class ViewController: UIViewController,	WKNavigationDelegate {
 
     func startLocalServer() {
         guard let webDir = Bundle.main.resourceURL?.appendingPathComponent("web") else {
-            print("Could not find web directory")
+            showError("Could not find web directory")
             return
         }
 
-server = HttpServer()
+        server = HttpServer()
 
-server["/"] = { _ in
-    let indexPath = webDir.appendingPathComponent("index.html").path
-    if let content = try? String(contentsOfFile: indexPath, encoding: .utf8) {
-        return HttpResponse.ok(.html(content))
-    }
-    return HttpResponse.notFound
-}
+        server["/"] = { _ in
+            let indexPath = webDir.appendingPathComponent("index.html").path
+            if let content = try? String(contentsOfFile: indexPath, encoding: .utf8) {
+                return HttpResponse.ok(.html(content))
+            }
+            return HttpResponse.notFound
+        }
 
-server["/(.*)"] = shareFilesFromDirectory(webDir.path)
+        server["/(.*)"] = shareFilesFromDirectory(webDir.path)
 
         do {
             try server.start(8080, forceIPv4: true)
-            print("Server started on port 8080")
         } catch {
-            print("Server failed to start: \(error)")
+            showError("Server failed to start: \(error.localizedDescription)")
         }
     }
-/** 
-    func loadGame() {
-        if let url = URL(string: "http://localhost:8080/") {
-            let request = URLRequest(url: url)
-            webView.load(request)
-        }
-    }
-**/
+
     func loadGame() {
         if let url = URL(string: "http://localhost:8080/") {
             let request = URLRequest(url: url)
             webView.load(request)
         } else {
             showError("Failed to create URL")
-        } 
+        }
     }
 
     func showError(_ message: String) {
-        let label = UILabel()
-        label.text = message
-        label.textColor = .white
-        label.textAlignment = .center
-        label.frame = view.bounds
-        view.backgroundColor = .black
-        view.addSubview(label)
+        DispatchQueue.main.async {
+            let label = UILabel()
+            label.text = message
+            label.textColor = .white
+            label.textAlignment = .center
+            label.numberOfLines = 0
+            label.frame = self.view.bounds
+            self.view.backgroundColor = .black
+            self.view.addSubview(label)
+        }
     }
 
-func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
-    showError("Failed to load: \(error.localizedDescription)")
-}
+    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+        showError("Failed to load: \(error.localizedDescription)")
+    }
 
-func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
-    showError("Navigation failed: \(error.localizedDescription)")
-}
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        showError("Navigation failed: \(error.localizedDescription)")
+    }
 
     override var prefersStatusBarHidden: Bool {
         return true
@@ -110,10 +91,5 @@ func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError 
 
     deinit {
         server?.stop()
-    }
-}
-extension ViewController: WKScriptMessageHandler {
-    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-        print("JS: \(message.body)")
     }
 }
